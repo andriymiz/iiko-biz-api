86b0ea4f912b893d938e341b93658b0286271e3e
Andriy Miz <andriy.miz.v@gmail.com>
2021-01-16 21:47:48 +0200

    remove demo credentials from config file

--- src/config/iiko-biz.php
+++ src/config/iiko-biz.php
@@ -4,7 +4,7 @@ return [
     
     'api_base_uri' => env('IIKO_BIZ_API_BASE_URI', 'https://iiko.biz:9900/api/0/'),
 
-    'user_id' => env('IIKO_BIZ_USER_ID', 'demoDelivery'),
+    'user_id' => env('IIKO_BIZ_USER_ID', ''),
 
-    'user_secret' => env('IIKO_BIZ_USER_SECRET', 'PI1yFaKFCGvvJKi'),
+    'user_secret' => env('IIKO_BIZ_USER_SECRET', ''),
 ];
